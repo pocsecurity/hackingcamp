a0057f6177362114b5749b4e880385de33dfcacb
Jerry Lee <z3rrry@gmail.com>
2024-12-09 17:24:38 +0900

    notice 로직 추가

---
+++ js/notice.js
@@ -0,0 +1,17 @@
+$(function() {
+    const $container = $('header .notice-container');
+    $.getJSON('./data/notice.json', function(resp) {
+        resp.forEach((e, i) => {
+            if (e.view) {
+                console.log(e)
+                const $notice = $(`
+                <div class="notice">
+                    <div class="notice-head">${e.date}</div>
+                    <div class="notice-body">${e.message.join('<br />')}</div>
+                </div>
+                `);
+                $container.append($notice);
+            }
+        })
+    })
+})
