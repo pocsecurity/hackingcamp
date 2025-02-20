ffe2003c7a16d90f69c2f5ff690a464e8ac3b9e1
Jerry Lee <z3rrry@gmail.com>
2025-02-20 17:35:35 +0900

    30회내용 업데이트 겸 소소한 UI 변경

--- js/history.js
+++ js/history.js
@@ -1,6 +1,8 @@
 $(function() {
     const $container = $('section#history .history-container');
     $.getJSON('./data/history.json', function(resp) {
+        let last_no = 0;
+        last_no = resp.length;
         resp.forEach((e,i) => {
             const $notice = $(`
             <div id="history${i+1}" class="history collapse ${(i+1==resp.length)?"in": ""}">
@@ -10,55 +12,39 @@ $(function() {
                         <p>${e.date}</p>
                         <p>${e.venue}</p>
                         ${(e.description)?`<p>${e.description}</p>`:''}
+                        <hr />
                     </div>
-                    <hr />
-                    <div class="history-schedule">
-                        <ul>
-                            ${
-                                e.schedule.map((s, i) => {
-                                    let code = (s.author)?`<span>${s.author}</span><br />`:'';
-                                    if (s.file) {
-                                        file_url = `slides/${e.no}/${s.file}`;
-                                        code += `<a href="${file_url}">${s.title}</a>`
-                                    } else {
-                                        code += s.title;
-                                    }
-                                    return `<li>${code}</li>`;
-                                }).join('')
-                            }
-                        </ul>
+                    <div class="history-body1">
+                        <div class="history-section schedule">
+                            ${ (e.schedule && e.schedule.length != 0)
+                                ?`<h2>발표</h2>${generate_schedule_list(e.no, e.schedule)}`
+                                :"" }
+                        </div>
+                        <div class="history-section award">
+                            ${ (e.awards && e.awards.length != 0)
+                                ?`<h2>수상자</h2>${generate_award_list(e.no, e.awards)}`
+                                :"" }
+                        </div>
                     </div>
-                    <div class="history-sponsor">
-                        ${
-                            (e.sponsor.length != 0)?"<hr /><h2>후원</h2>":""
-                        }
-                        <ul>
-                            ${
-                                e.sponsor.map((s, i) => {
-                                    return `
-                                    <li>
-                                        <a href="${s.site}" target="_blank"><img src="${s.logo_link}" /></a>
-                                        <p>${s.description}</p>
-                                    </li>`
-                                }).join('')
-                            }
-                        </ul>
+                    <div class="history-section sponsor">
+                        ${ (e.sponsor && e.sponsor.length != 0)
+                            ?`<h2>후원</h2>${generate_sponsor_list(e.no, e.sponsor)}`
+                            :"" }
                     </div>
                 </div>
             </div>
             `);
             $container.append($notice);
         })
+        generate_history_btns(last_no);
     });
 
-    generate_history_btns();
 })
 
 
 
-function generate_history_btns() {
+function generate_history_btns(last_no) {
     // [지난 해킹캠프] 회차 번호 버튼을 생성.
-    const last_no = 29;
     const btn_fmt = '<button class="btn btn-default collapsed" type="button" data-toggle="collapse" data-target="#history{0}" aria-expanded="false" aria-controls="history{0}">{0}회</button>';
     for (let i = 1; i <= last_no; i++) {
         let btn_code = btn_fmt.replace(/\{0\}/g, i);
@@ -70,11 +56,70 @@ function generate_history_btns() {
     // 모두 닫기 버튼
     let $close_all_btn = $(`<button class="btn btn-default collapsed"><i class="fa-solid fa-rotate-left"></i></button>`);
     $close_all_btn.on('click', function() {
-        let $opened = $('div.in')
+        let $opened = $('#history div.in')
         $opened.removeClass('in');
         $opened.addClass('collapse');
 
         $('.history-btn-container .btn:not(.collapsed)').addClass('collapsed')
     })
+    // 모두 열기 버튼
+    let $open_all_btn = $(`<button class="btn btn-default collapsed"><i class="fa-solid fa-angles-down"></i></button>`);
+    $open_all_btn.on('click', function() {
+        let $opened = $('#history div.collapse')
+        $opened.removeClass('collapse');
+        $opened.addClass('in');
+
+        $('.history-btn-container .btn.collapsed').removeClass('collapsed');
+    })
     $('.history-btn-container').append($close_all_btn);
+    $('.history-btn-container').append($open_all_btn);
+}
+
+function generate_schedule_list(no, schedule) {
+    // 일정 리스트업
+    return `<ul>
+        ${
+            schedule.map((s, i) => {
+                let code = (s.author)?`<span>${s.author}</span><br />`:'';
+                if (s.file) {
+                    file_url = `slides/${no}/${s.file}`;
+                    code += `<a href="${file_url}">${s.title}</a>`
+                } else {
+                    code += s.title;
+                }
+                return `<li>${code}</li>`;
+            }).join('')
+        }
+        </ul>`;
+}
+
+function generate_award_list(no, awards) {
+    // 수상자 리스트업
+    return `<ul>
+        ${
+            awards.map((a, i) => {
+                return `
+                <li>
+                    <p><b>${a.type}</b></p>
+                    <span>${a.affiliation}</span>
+                    <p class="name">${a.name}</p>
+                </li>`
+            }).join('')
+        }
+        </ul>`;
+}
+
+function generate_sponsor_list(no, sponsors) {
+    // 후원사 리스트업
+    return `<ul>
+        ${
+            sponsors.map((s, i) => {
+                return `
+                <li>
+                    <a href="${s.site}" target="_blank"><img src="${s.logo_link}" /></a>
+                    <p>${s.description}</p>
+                </li>`
+            }).join('')
+        }
+        </ul>`;
 }
---
+++ js/prepare.js
@@ -0,0 +1,138 @@
+$(function() {
+    $('.preparing figure pre').html(
+`🔥 첫째 날
+        ~ 10:00  ㅁㅁ ㅁ ㅁㅁ ㅁㅁ
+10:00 ~ 10:30  ㅁㅁㅁㅁㅁ ㅁㅁㅁ
+10:30 ~ 12:00  ㅁ ㅁㅁ ㅁ ㅁ ㅁㅁ(ㅁㅁ ㅁㅁㅁ, ㅁㅁㅁ ㅁ)
+12:00 ~ 13:00  ㅁㅁㅁㅁ
+13:00 ~ 13:40  ㅁ ㅁㅁ ㅁㅁㅁㅁㅁㅁ ㅁ ㅁㅁㅁ ㅁㅁㅁ - ㅁㅁㅁ
+13:40 ~ 13:50  ㅁㅁㅁㅁ
+13:50 ~ 14:30  ㅁㅁㅁㅁㅁㅁㅁ ㅁㅁㅁㅁ! - ㅁㅁ
+14:30 ~ 15:00  ㅁㅁㅁ
+15:00 ~ 15:40  ㅁㅁㅁㅁ ㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁ - ㅁㅁ
+15:40 ~ 16:40  ㅁㅁ/ㅁㅁ ㅁㅁ ㅁㅁ
+16:40 ~ 17:20  ㅁㅁㅁ ㅁㅁㅁ ㅁㅁㅁㅁ ㅁㅁㅁㅁ ㅁㅁㅁㅁ - ㅁㅁㅁ
+17:20 ~ 17:30  ㅁㅁㅁㅁ
+17:30 ~ 18:00  ㅁㅁㅁ ㅁㅁㅁ
+18:00 ~ 19:00  ㅁㅁㅁㅁ ㅁㅁㅁ
+19:00 ~ 21:00  ㅁㅁㅁㅁ-1
+21:00 ~ 25:00  ㅁㅁㅁㅁ-2
+
+🚀 둘째 날 
+    ~ 09:30  ㅁㅁㅁㅁ, ㅁㅁ, ㅁㅁ
+09:30 ~ 10:00  ㅁㅁ(ㅁㅁㅁㅁ)
+10:00 ~ 10:40  ㅁㅁㅁ ㅁㅁ ㅁㅁㅁㅁ - ㅁㅁㅁ
+10:40 ~ 11:20  ㅁㅁㅁ
+11:20 ~ 12:00  ㅁㅁ ㅁㅁㅁㅁ ㅁㅁㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁㅁㅁㅁ - ㅁㅁㅁ
+12:00 ~ 13:00  ㅁㅁㅁㅁ
+13:00 ~ 13:40  ㅁㅁ ㅁㅁ ㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁㅁㅁㅁ - ㅁㅁㅁ
+13:40 ~ 13:50  ㅁㅁㅁㅁ
+13:50 ~ 14:30  ㅁㅁㅁ! ㅁㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁㅁㅁ - ㅁㅁㅁ
+14:30 ~ 15:10  ㅁㅁㅁ
+15:10 ~ 15:20  ㅁㅁㅁㅁ
+15:20 ~ 16:00  ㅁㅁㅁㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁ ㅁㅁㅁㅁㅁ ㅁㅁㅁ - ㅁㅁㅁ
+16:00 ~ 16:10  ㅁㅁㅁㅁ
+16:10 ~ 17:10  ㅁㅁㅁ ㅁㅁㅁㅁ
+17:10 ~ 18:00  🏆 ㅁㅁㅁ, ㅁㅁㅁ, ㅁㅁㅁ`);
+        
+    $('.preparing .featured-box .venue').html(`
+        <h4>날짜 및 장소</h4>
+        <p>* 날짜: 1998년 12월 18일(ㅁ) ~ 00일(ㅁ)</p>	
+        <p>* 집결 장소: ㅁㅁ ㅁㅁㅁ ㅁㅁㅁㅁㅁㅁㅁㅁㅁ / 10:00까지</p>
+        <p>* ㅁㅁㅁㅁㅁㅁㅁㅁ로 집결 및 귀가는 개별적으로 하며, 교통편은 유스호스텔 홈페이지 참고</p>
+    `);
+    $('.preparing .featured-box .notice').html(`
+        <h4>참고사항</h4>
+        <p>* 21:00 이후부터는 팀별 방에서 해킹캠프 CTF-2를 진행</p> 
+        <p>* CTF-2 시간은 상황에 따라 조정될 수 있음</p>
+        <p>* 시간표는 사정에 따라 변경될 수 있습니다</p>
+    `);
+    $('.preparing .featured-box .sponsor').html(`
+        <h4>후원사</h4>
+        <a target="_blank"><img src="img/pocsec.png" width="210" /></a>
+        <p>ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
+        <br />
+    `);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//               <div class="featured-box">
+//                   <div class="featured-box-col1 wow fadeInRight delay-02s">
+//                       <i class="fa-volume-up"></i>
+//                   </div>
+//                   <div class="featured-box-col2 wow fadeInRight delay-02s">
+//                       <h4>날짜 및 장소</h4>
+//                       <p>* 날짜: 2025년 2월 15일(토) ~ 16일(일)</p>	
+//           <p>* 집결 장소: 서울 영등포 하이서울유스호스텔 / 10:00까지</p>
+//           <p>* 하이서울유스호스텔로 집결 및 귀가는 개별적으로 하며, 교통편은 <a href="http://hiseoulyh.com/ko" target="_blank">하이서울유스호스텔 홈페이지</a> 참고  </p>
+//                   </div>    
+
+//               </div>
+
+//               <div class="featured-box">
+//                   <div class="featured-box-col1 wow fadeInRight delay-04s">
+//           <i class="fa-regular fa-comment"></i>
+//                   </div>
+//                   <div class="featured-box-col2 wow fadeInRight delay-04s">
+//                       <h4>참고사항</h4>
+//                       <p>* 21:00 이후부터는 팀별 방에서 해킹캠프 CTF-2를 진행</p> 
+//                       <p>* CTF-2 시간은 상황에 따라 조정될 수 있음</p>
+//                       <p>* 시간표는 사정에 따라 변경될 수 있습니다</p>
+//                   </div>    
+
+//               </div>
+
+//               <div class="featured-box">
+//                   <div class="featured-box-col1 wow fadeInRight delay-06s">
+//                       <i class="fa fa-life-ring"></i>
+//                   </div>
+//                   <div class="featured-box-col2 wow fadeInRight delay-06s">
+//                       <h4>후원사</h4>
+//                       <a href="https://x.com/fuzzySec/"><p style="font-size: 21px; font-weight:bold; color: black; margin-bottom: 0.5rem">Ruben Boonen</p></a>
+//                       <p>Ruben Boonen (<a href="https://x.com/fuzzySec">@FuzzySec</a>)은 IBM 소속 연구원으로, 세계 각국의 보안 컨퍼런스에서 발표하며 보안 업계에 큰 영향력을 미치고 있습니다. 그는 Valentina Palmiotti (<a href="https://x.com/chompie1337">@Chompie1337</a>)와 함께 30회 해킹캠프의 CTF 문제 출제에도 도움을 주었습니다.</p>
+//                       <br />
+
+//                       <a href="https://hayyimlab.oopy.io/" target="_blank"><img src="img/sponsor/hayyim.png" width="200" /></a>
+//                       <p>오펜시브 최중요 연구 과제 수행 및 해킹 트렌드를 선도하는 기업입니다. 학생들이 실제 보안 연구 경험을 쌓을 수 있도록 장학금 및 연구 지원 제도를 운영하고 있습니다.</p>
+//                       <br />
+
+//                       <a href="https://www.piolink.com/kr/" target="_blank"><img src="img/sponsor/piolink.png" width="200" /></a>
+//                       <p>파이오링크는 업계 최고의 애플리케이션 가용성, 성능, 보안을 제공하고, 쉬운 관리를 위한 네트워크 및 보안 제품 개발에 주력하고 있습니다. 또한 차별화된 보안관제 서비스와 보안컨설팅 그리고 보안관리 솔루션을 기반으로 전문화된 정보보호 서비스를 제공하고 있습니다.</p>
+//                       <br />
+
+//                       <a href="https://www.facebook.com/secleg" target="_blank"><img src="img/sponsor/kiboa.png" width="210" /></a>
+//                       <p>키다리 보안 아저씨는 비수도권 지역의 정보 보안 교육 기회를 확대하여 정보 불균형을 해소하는 것을 목표로, 행사 및 멘토링 등 다양한 형태의 프로그램을 운영하여 보안 업계에 긍정적인 변화를 만들어가고 있습니다.</p>
+
+//                       <!-- <br />
+//                       <p>후원사 모집중</p>
+//                       <p>(2025. 01. 31 까지)</p>
+//                       <p>후원 문의: <a href="mailto:h4ckingc4mp@gmail.com">h4ckingc4mp@gmail.com</a></p> -->
+
+//                   </div>  
+
+
+//               </div>
+
+//               <a class="Learn-More" href="#">Learn More</a>
+
+//           </div>
+//         </div>
+//         `);
+})
